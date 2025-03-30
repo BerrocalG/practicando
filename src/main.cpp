b2819d5e640964f9c60796e7ec448073eb146b64
BerrocalG <guadalupe.berrocal@usil.pe>
2025-03-30 10:53:13 -0500

    tarea 1 parte 2  completa

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,23 @@
 #include <util/delay.h>  
 
 int main(void) {  
-    DDRD |= 0xFE;  // Configura PD0 a PD5 como salidas (dos semáforos)  
+    DDRD |= 0xFE;  // Configura PD0 a PD5 como salidas (dos semáforos) 
 
     while(1) {  
-        // **FASE 1: Semáforo 1 en ROJO, Semáforo 2 en azul**  
+        // **FASE 1: Semáforo 1 en ROJO, Semáforo 2 en verde  
         PORTD = 0x84;  
         _delay_ms(3000);   
 
-        // **FASE 3: Semáforo 1 en VERDE, Semáforo 2 en ROJO**  
+        // **FASE 3: Semáforo 1 en AMARILLO, Semáforo 2 en VERDE   
         PORTD = 0x44;   
-        _delay_ms(4000);  
+        _delay_ms(1000);  
 
-        // **FASE :  
+        // **FASE :  semaforo 1 en verde; semaforo 2 en rojo
         PORTD = 0x30;   
         _delay_ms(3000); 
 
-        // **FASE :  
-        PORTD = 0x30;   
-        _delay_ms(3000);  
-
-       
-    }  
+        // **FASE :  Semaforo 1 en verde; semaforo 2 en amarillo
+        PORTD = 0x28;   
+        _delay_ms(1000); //
+    }   
 }
